327dfd68ed275b0a45728e0f9eb70ccb61acf639
xradziox <31067@student.pwsztar.edu.pl>
2018-12-10 18:11:56 +0100

    angular

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -8,6 +8,33 @@ import { MapComponent } from './components/map/map.component';
 import { QuizIndexComponent } from './components/quiz-index/quiz-index.component';
 import { QuizGameComponent } from './components/quiz-game/quiz-game.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'map',
+    component: MapComponent,
+  },
+  {
+    path: 'quiz-index',
+    component: QuizIndexComponent,
+  },
+  {
+    path: 'quiz-game',
+    component: QuizGameComponent,
+  },
+  {
+    path: 'nav-bar',
+    component: NavBarComponent,
+  }];
 
 @NgModule({
   declarations: [
@@ -20,9 +47,17 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
     NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
+
+
 })
-export class AppModule { }
+export class AppModule {
+}
+
+
+
+
